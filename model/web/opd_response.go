6bb90299344c4c2f1ca4ca9d3d50880eda74c99a
Affandi Pratama <affandipratama45@gmail.com>
2026-04-06 15:31:50 +0700

    Update struktur response opd

--- model/web/opd_response.go
+++ model/web/opd_response.go
@@ -4,8 +4,8 @@ type OpdResponse struct {
 	Id                           int    `json:"id,omitempty"`
 	KodeOpd                      string `json:"kode_opd"`
 	NamaOpd                      string `json:"nama_opd"`
-	KodeLembaga                  string `json:"kode_lembaga"`
 	NamaKepalaPerangkatDaerah    string `json:"nama_kepala_perangkat_daerah"`
 	NipKepalaPerangkatDaerah     string `json:"nip_kepala_perangkat_daerah"`
 	PangkatKepalaPerangkatDaerah string `json:"pangkat_kepala_perangkat_daerah"`
+	KodeLembaga                  string `json:"kode_lembaga"`
 }
